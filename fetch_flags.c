997c45250421f6e15bff6e8fef0ad35176263222
lawrence1986 <madusquare@gmail.com>
2023-04-22 14:58:13 -0700

    hot fix fetc_flags.c

--- fetch_flags.c
+++ fetch_flags.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* get_flags - Calculates active flags
+* fetch_flags - Calculates active flags
 * @format: Formatted string in which to print the arguments
-* @i: take a parameter.
+* @k: take a parameter.
 * Return: Flags:
 */
 int fetch_flags(const char *format, int *k)
