187594b1f4502bad2ca9d9bdb31d8a0a39281fa0
lawrence1986 <madusquare@gmail.com>
2023-04-22 13:48:50 -0700

    binary_convert.c update

--- binary_convert.c
+++ binary_convert.c
@@ -29,7 +29,7 @@ return (write_unsgnd(0, k, buffer, flags, width, precision, size));
 }
 /*** PRINT UNSIGNED NUMBER IN OCTAL ***/
 /**
-* print_octal - Prints an unsigned number in octal notation
+* print_oct - Prints an unsigned number in octal notation
 * @set: Lista of arguments
 * @buffer: Buffer array to handle print
 * @flags: Calculates active flags
@@ -61,7 +61,7 @@ return (write_unsgnd(0, k, buffer, flags, width, precision, size));
 }
 /*** PRINT UNSIGNED NUMBER IN HEXADECIMAL ***/
 /**
-* print_hexadecimal - Prints an unsigned number in hexadecimal notation
+* print_x - Prints an unsigned number in hexadecimal notation
 * @set: Lists all of arguments
 * @buffer: Buffer array to handle print
 * @flags: Calculates active flags
@@ -78,7 +78,7 @@ flags, 'x', width, precision, size));
 }
 /*** PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL ***/
 /**
-* print_hexa_upper - Prints an unsigned number in upper hexadecimal notation
+* print_xUpper - Prints an unsigned number in upper hexadecimal notation
 * @set: Lists of arguments
 * @buffer: Buffer array to handle print
 * @flags: Calculates active flags
@@ -95,7 +95,7 @@ flags, 'X', width, precision, size));
 }
 /*** PRINT HEXX NUM IN LOWER OR UPPER ***/
 /**
-* print_hexa - Prints a hexadecimal number in lower or upper
+* print_xd - Prints a hexadecimal number in lower or upper
 * @set: Lista of arguments
 * @map_to: Array of values to map the number to
 * @buffer: Buffer array to handle print
