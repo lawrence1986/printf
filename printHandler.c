41be0e3ca88536bfa6677b090dc956899c154b9c
lawrence1986 <madusquare@gmail.com>
2023-04-22 16:16:20 -0700

    printHandler.c

--- printHandler.c
+++ printHandler.c
@@ -1,31 +1,31 @@
 #include "main.h"
 /**
-* handle_print - Prints an argument based on its type
+* printHandler - Prints an argument based on its type
 * @fmt: Formatted string in which to print the arguments.
-* @list: List of arguments to be printed.
+* @file: List of arguments to be printed.
 * @ind: ind.
 * @buffer: Buffer array to handle print.
 * @flags: Calculates active flags
 * @width: get width.
-* @precision: Precision specification
+* @clarity: Precision specification
 * @size: Size specifier
 * Return: 1 or 2;
 */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+int printHandler(const char *fmt, int *ind, va_list file, char buffer[],
 int flags, int width, int precision, int size)
 {
-int i, unknow_len = 0, printed_chars = -1;
-fmt_t fmt_types[] = {
+int k, unknow_len = 0, output_chars = -1;
+fmt_t fmt_set[] = {
 {'c', print_char}, {'s', print_string}, {'%', print_percent},
-{'i', print_int}, {'d', print_int}, {'b', print_binary},
-{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+{'k', print_int}, {'d', print_int}, {'b', print_binary},
+{'u', print_unt}, {'o', print_oct}, {'x', print_x},
+{'X', print_xUpper}, {'p', print_ptr}, {'S', print_legible},
+{'r', print_rev}, {'R', print_rot13str}, {'\0', NULL}
 };
-for (i = 0; fmt_types[i].fmt != '\0'; i++)
-if (fmt[*ind] == fmt_types[i].fmt)
-return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
-if (fmt_types[i].fmt == '\0')
+for (k = 0; fmt_set[k].fmt != '\0'; k++)
+if (fmt[*ind] == fmt_set[k].fmt)
+return (fmt_set[k].fn(file, buffer, flags, width, precision, size));
+if (fmt_set[k].fmt == '\0')
 {
 if (fmt[*ind] == '\0')
 return (-1);
@@ -44,5 +44,5 @@ return (1);
 unknow_len += write(1, &fmt[*ind], 1);
 return (unknow_len);
 }
-return (printed_chars);
+return (output_chars);
 }
