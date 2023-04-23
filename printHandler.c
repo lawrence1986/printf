788968a065c91e0e55e390895eb63659cb85b2de
lawrence1986 <madusquare@gmail.com>
2023-04-23 13:26:22 -0700

    printHandler.c

--- printHandler.c
+++ printHandler.c
@@ -7,7 +7,7 @@
 * @buffer: Buffer array to handle print.
 * @flags: Calculates active flags
 * @width: get width.
-* @clarity: Precision specification
+* @precision: Precision specification
 * @size: Size specifier
 * Return: 1 or 2;
 */
