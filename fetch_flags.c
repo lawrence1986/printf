5e4000af051fc70d384c930dfcf92990d79813c7
lawrence1986 <madusquare@gmail.com>
2023-04-23 16:50:09 -0700

    bugs

--- fetch_flags.c
+++ fetch_flags.c
@@ -2,22 +2,22 @@
 /**
 * fetch_flags - Calculates active flags
 * @format: Formatted string in which to print the arguments
-* @k: take a parameter.
+* @i: take a parameter.
 * Return: Flags:
 */
-int fetch_flags(const char *format, int *k)
+int fetch_flags(const char *format, int *i)
 {
 /* - + 0 # ' ' */
 /* 1 2 4 8 16 */
-int j, curr_k;
+int j, curr_i;
 int flags = 0;
 const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH,
 F_SPACE, 0};
-for (curr_k = *k + 1; format[curr_k] != '\0'; curr_k++)
+for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 {
 for (j = 0; FLAGS_CH[j] != '\0'; j++)
-if (format[curr_k] == FLAGS_CH[j])
+if (format[curr_i] == FLAGS_CH[j])
 {
 flags |= FLAGS_ARR[j];
 break;
@@ -25,6 +25,6 @@ break;
 if (FLAGS_CH[j] == 0)
 break;
 }
-*k = curr_k - 1;
+*i = curr_i - 1;
 return (flags);
 }
