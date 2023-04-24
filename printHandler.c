40c1268f730df6b9831fc876362fb362a88b77e1
codeabuu <abdulngui718@gmail.com>
2023-04-24 05:10:34 -0700

    newchanges

--- printHandler.c
+++ printHandler.c
@@ -14,7 +14,7 @@
 int printHandler(const char *fmt, int *ind, va_list file, char buffer[],
 int flags, int width, int precision, int size)
 {
-int i, unknow_len = 0, output_chars = -1;
+int i = 0, unknow_len = 0, output_chars = -1;
 fmt_t fmt_set[] = {
 {'c', print_char}, {'s', print_string}, {'%', print_percent},
 {'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -22,9 +22,13 @@ fmt_t fmt_set[] = {
 {'X', print_xUpper}, {'p', print_ptr}, {'S', print_legible},
 {'r', print_rev}, {'R', print_rot13str}, {'\0', NULL}
 };
-for (i = 0; fmt_set[i].fmt != '\0'; i++)
+
+while (fmt_set[i].fmt != '\0')
+{
 if (fmt[*ind] == fmt_set[i].fmt)
 	return (fmt_set[i].fn(file, buffer, flags, width, precision, size));
+i++;
+}
 if (fmt_set[i].fmt == '\0')
 {
 if (fmt[*ind] == '\0')
