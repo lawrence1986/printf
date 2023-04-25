459a93aef0648b18eac139550b4af4e99db9326b
codeabuu <abdulngui718@gmail.com>
2023-04-25 14:38:51 -0700

    size matters :)

--- dataSize.c
+++ dataSize.c
@@ -1,33 +1,26 @@
 #include "main.h"
 
 /**
-* dataSize - Calcs size to cast argument
-* @format: Formatted string in which to print the arguments
-* @i: List of arguments to be printed.
-*
-* Return: Size.
-*/
+ * dataSize - Calculates the size to cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ *
+ * Return: Precision.
+ */
 int dataSize(const char *format, int *i)
 {
-int curr_i = *i + 1;
-int size = 0;
+	int curr_i = *i + 1;
+	int size = 0;
 
-switch (format[curr_i])
-{
-case 'l':
-size = S_LONG;
-break;
-case 'h':
-size = S_SHORT;
-break;
-default:
-*i = curr_i - 1;
-break;
+	if (format[curr_i] == 'l')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
+
+	return (size);
 }
-
-if (size != 0)
-*i = curr_i;
-
-return (size);
-}
-
